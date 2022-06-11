7cb72c4f50eb8fd2c800520c959ae752a1f06567
Meherab <jubayet0106@gmail.com>
2022-06-11 17:15:50 +0600

    product page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http'); 
 const fs = require('fs'); 
+const url = require('url');
 const replaceTemplate = require('./Components/replace');
 
 
@@ -10,14 +11,19 @@ const data = fs.readFileSync('./Data/Data.json', 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res)=>{
-    const pathName = req.url;
-    if (pathName === '/' || pathName === "/overview") {
+    const { query, pathname } = url.parse(req.url, true);
+    if (pathname === '/' || pathname === "/overview") {
         res.writeHead(200, {contentType: 'text/html'});
         const cardsHtml = dataObj.map(el => replaceTemplate(cardData, el)).join('');
         const output = overviewData.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
-    } else if (pathName === "/product") {
-        res.end("hello from product");
+    } else if (pathname === "/product") {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(ProductData, product);
+        res.end(output);
     } else{
         res.end("found nothing")
     };
